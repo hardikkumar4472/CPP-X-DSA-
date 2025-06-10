74421405e6f740e15b999cd6a6d9498e4c83fc9e
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-06-10 22:42:14 +0530

    Add files via upload

---
+++ 2. Variables & Data Types/e.cpp
@@ -0,0 +1,5 @@
+//Namespace-> to avoid name conflicts
+using namespace std;
+//Cin, Cout-> Exist only in iostream header file & namespace
+//Declare to just initialize just tell
+//Define means to initialize and assign value explain in brief
