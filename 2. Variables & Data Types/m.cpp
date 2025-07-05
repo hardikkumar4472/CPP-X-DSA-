b138442c435ccf2a624fdc1f32a9f1fd49163ff9
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-07-05 23:46:49 +0530

    Add files via upload

---
+++ 2. Variables & Data Types/m.cpp
@@ -0,0 +1,12 @@
+//Input in c++
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b; //Input two numbers
+    cout<<"Sum is: "<<a+b<<endl; //Output the sum of the two numbers
+    return 0;
+}
+//Activity Sum of two numbers
+//Input two numbers and output the sum of the two numbers
