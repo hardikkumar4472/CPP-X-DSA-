b0c5d9ef79cdf2f34b891d13b5a68721c3673297
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-07-05 23:43:56 +0530

    Add files via upload

---
+++ 2. Variables & Data Types/i.cpp
@@ -0,0 +1,8 @@
+// Can start with _ variables
+// Can contain letters, digits and underscore
+// Can not contain special characters like @, #, $, %, ^, &, *, (, ), !, ?, /, \, etc
+// Can not contain space
+// Can not start with digit
+// Can not be same as keyword
+// Can not be same as any other variable name in the same scope
+//Upperand lower case letters are different in c++ and can be there in variables
