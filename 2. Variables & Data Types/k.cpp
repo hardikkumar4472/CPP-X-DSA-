e3f0278652107ca913a043c203aa1d40fa480e24
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-07-05 23:44:49 +0530

    Add files via upload

---
+++ 2. Variables & Data Types/k.cpp
@@ -0,0 +1,14 @@
+//Data Types
+//Primitive int char bool float double
+//Non Primitive array string structure class
+// int 4 bytes, 32 bits
+// char 1 byte, 8 bits
+// bool 1 byte, 8 bits
+// float 4 bytes, 32 bits
+// double 8 bytes, 64 bits
+// string 1 byte, 8 bits
+// array 1 byte, 8 bits
+// structure 1 byte, 8 bits
+// class 1 byte, 8 bits
+// Sizeof() is used to find size of data type in bytes
+// Setprecision() is used to set the precision of float and double values
