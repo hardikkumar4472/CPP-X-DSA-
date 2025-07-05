4f82d78658a64296af7be61f1a09a895a0409d41
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-07-05 23:43:26 +0530

    Add files via upload

---
+++ 2. Variables & Data Types/h.cpp
@@ -0,0 +1,8 @@
+// Variables in c++, variable are the name of memeber location in memory
+//Variable are used to store data in memory
+// variable names are the identifier of memory location
+// Name should be meaningful and should not be same as keyword
+//Variable name should not start with digit and should not contain space
+// data is stored in memory in binary form (RAM)
+//Acts as plots on big lan like squares in a rectangle
+// printing variable will throw garbage value of value is not assigned to it
