45ce5e008d2f8317bac29a7a70b8ccabcdc46151
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-07-06 23:23:57 +0530

    DSA

---
+++ 3. Operator/e.cpp
@@ -0,0 +1,13 @@
+//Arithmetic Operators-> +,-,*,/,%
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 10;
+    int b = 3;
+    cout << "Addition: " << a + b << endl; // 13
+    cout << "Subtraction: " << a - b << endl; // 7
+    cout << "Multiplication: " << a * b << endl; // 30
+    cout << "Division: " << a / b << endl; // 3
+    cout << "Modulus: " << a % b << endl; // 1
+    return 0;
+}
