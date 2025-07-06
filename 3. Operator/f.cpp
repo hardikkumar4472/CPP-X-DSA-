e626f344459ce11f9b679f3cc1849967cb706b91
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-07-06 23:24:17 +0530

    DSA

---
+++ 3. Operator/f.cpp
@@ -0,0 +1,10 @@
+//Unary operator
+#include <iostream>
+using namespace std;
+int main(){
+    int a = 5;
+    cout << "Increment: " << ++a << endl; // 6
+    cout << "Decrement: " << --a << endl; // 4
+    cout << "Negation: " << -a << endl; // -4
+    return 0;
+}
