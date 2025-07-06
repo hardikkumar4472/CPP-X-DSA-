c92bb9fd0aec4bb92d92bd06201333f19ad1001c
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-07-06 23:25:21 +0530

    DSA

---
+++ 3. Operator/i.cpp
@@ -0,0 +1,12 @@
+//Logical Operators-> Convert to binary and result in decimal
+// Logical Operators
+#include <iostream>
+using namespace std;
+int main(){
+    bool a = 4;
+    bool b = 5;
+    cout << "Logical AND: " << (a && b) << endl; 
+    cout << "Logical OR: " << (a || b) << endl; 
+    cout << "Logical NOT: " << (!a) << endl; 
+    return 0;
+}
