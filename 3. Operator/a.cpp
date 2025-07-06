bcddab45783e96404540844b5720ed356c829a56
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-07-06 23:20:33 +0530

    DSA

---
+++ 3. Operator/a.cpp
@@ -0,0 +1,19 @@
+//constants->variables that can't be changed 
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     const int a = 5; //constant variable
+//     cout << a << endl;
+//     //a = 10; //error: assignment of read-only variable 'a'
+//     //const int b; //error: uninitialized const 'b'
+//     const int c = 10;
+//     cout << c << endl;
+//     return 0;
+// }
+#include <iostream>
+using namespace std;
+#define ll long long long;
+int main(){
+    ll b=5;
+    cout << b << endl;
+}
