e2f74deb9f1a77923d5b002bd98aef34f5247507
Hardik kumar <140632249+hardikkumar4472@users.noreply.github.com>
2025-07-17 23:49:15 +0530

    DSA

---
+++ 6. Nested Loops Patterns/p9.cpp
@@ -0,0 +1,35 @@
+/*
+
+    *
+   ***
+  *****
+ *******
+  ******
+   ***
+    *
+
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cin>>a;
+    for (int i=1;i<=a;i++){
+        for (int j=1;j<=a-i;j++){
+            cout<<" ";
+        }
+        for (int j=0;j<=(2*i)-1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    for (int i=a;i>=1;i--){
+        for (int j=1;j<=a-i;j++){
+            cout<<" ";
+        }
+        for (int j=0;j<=(2*i)-1;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
